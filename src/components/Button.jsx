28ef35c0ada8646145833df94dce6a15d0cd0956
hectorbliss <contacto@fixter.org>
2020-06-12 12:33:09 -0500

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button(props){
+    return (
+        <button>
+            {props.text}
+        </button>
+    )
+}
+
+export default Button
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './Button'
+
+function Nav(){
+    return (
+        <nav>
+            <Button text="Home" />
+            <Button text="ya" />
+            <Button text="vamonos" />
+            <Button text="que " />
+            <Button text="aqui espantan" />
+        </nav>
+    )
+}
+
+export default Nav
